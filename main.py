52687e7e155390f4242320c8d5c08ca79fb15adc
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-01 05:03:11 -0400

    create snake animation

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # speedy snake game
 import random
 from turtle import Turtle, Screen
+import time
 
 # setup screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Speedy Snake")
+screen.tracer(0)
 
 # snake starter speed
 # speed will increase each time the snake eats the food
@@ -60,5 +62,30 @@ screen.onkeypress( right, "Right")
 screen.onkeypress( up, "Up")
 screen.onkeypress( left, "Left")
 screen.onkeypress( down, "Down")
+
+# snake animation
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(snake_speed)
+    snake_body.penup()
+    snake_body.forward(20)
+
+    # detect collision with food
+    if snake_body.distance(food) < 20:
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        food.goto(random_x, random_y)   
+        total_score += 1
+        scoreboard.clear()
+        scoreboard.write(f" Score: {total_score}", align="center", font=("Verdana", 8, "normal"))
+        snake_speed *= 0.9
+
+   # detect collision with wall
+    if snake_body.xcor() > 290 or snake_body.xcor() < -290 or snake_body.ycor() > 290 or snake_body.ycor() < -290:
+        is_game_on = False
+        scoreboard.goto(0,0)
+        scoreboard.write(f"Game Over", align="center", font=("Verdana", 12, "normal"))
 
 screen.exitonclick()
